7c1af6344759f6296c29f2ab5bca92b09f8bed4c
bncblnc <bncblnc@gmail.com>
2023-09-05 14:53:22 -0300

    up api

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
-    }
-})
+export const api = axios.create({
+  baseURL: 'https://famuunpwupdlqnpbwsmd.supabase.co/rest/v1/',
+  headers: {
+    apikey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhbXV1bnB3dXBkbHFucGJ3c21kIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzkzMzc5OSwiZXhwIjoyMDA5NTA5Nzk5fQ.RIqFv3Aq02-QYg7q9aZfvtDs46aVGe2GyfFWXgrAJJU',
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhbXV1bnB3dXBkbHFucGJ3c21kIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MzkzMzc5OSwiZXhwIjoyMDA5NTA5Nzk5fQ.RIqFv3Aq02-QYg7q9aZfvtDs46aVGe2GyfFWXgrAJJU',
+  },
+});
